ac0cddd2a4c1694a1a9a445ef46e66136df178f7
mlthelama <lackner.martin79@gmail.com>
2022-07-23 22:11:12 +0200

    added features for #2 and #3
    * added new translation key
    * changed UI to fit the new fields
    * added configuration value to move the window around

--- src/util/offset.h
+++ src/util/offset.h
@@ -1,6 +1,9 @@
 #pragma once
 
 namespace offset {
-    constexpr REL::ID get_relationship_rank{ 53898 };
+    constexpr REL::ID get_relationship_rank{ 53898 }; //54710 might be for AE, testing
     constexpr REL::ID get_dialog_menu{ 268589 }; //215255 AE
 };
+
+
+
