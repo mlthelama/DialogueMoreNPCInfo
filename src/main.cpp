43fc2cc1d3bfb02eaca47253f16cdb99a02d7c40
mlthelama <lackner.martin79@gmail.com>
2022-08-03 16:08:01 +0200

    changes for all in one build

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,23 @@
 #include "setting/setting.h"
 #include "util/constant.h"
 
-void MessageHandler(SKSE::MessagingInterface::Message* a_msg) {
-    switch (a_msg->type) {
-        case SKSE::MessagingInterface::kDataLoaded:
-            logger::info("Data loaded"sv);
-            hook::install_hooks();
-            event::sink_event_handlers();
-            scaleform::Register();
-            break;
+void init_logger() {
+    if (static bool initialized = false; !initialized) {
+        initialized = true;
+    } else {
+        return;
     }
-}
 
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info) {
     try {
 #ifndef NDEBUG
         auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
 #else
         auto path = logger::log_directory();
         if (!path) {
-            return false;
+            stl::report_and_fail("failed to get standard log path"sv);
         }
 
-        *path /= Version::PROJECT;
-        *path += ".log"sv;
+        *path /= fmt::format("{}.log"sv, Version::PROJECT);
         auto sink = make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
 #endif
         auto log = make_shared<spdlog::logger>("global log"s, move(sink));
@@ -41,38 +35,11 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
         set_default_logger(move(log));
         spdlog::set_pattern("[%H:%M:%S.%f] %s(%#) [%^%l%$] %v"s);
 
-        logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
+        logger::info("{} v{}"sv, Version::PROJECT, Version::NAME);
 
-        a_info->infoVersion = SKSE::PluginInfo::kVersion;
-        a_info->name = Version::PROJECT.data();
-        a_info->version = Version::ASINT;
-
-        if (a_skse->IsEditor()) {
-            logger::critical("Loaded in editor, marking as incompatible"sv);
-            return false;
-        }
-
-        const auto ver = a_skse->RuntimeVersion();
-        if (ver < SKSE::RUNTIME_1_5_39) {
-            logger::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
-            return false;
-        }
-    } catch (const std::exception& e) {
-        logger::critical("failed, cause {}"sv, e.what());
-        return false;
-    }
-
-    return true;
-}
-
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse) {
-    try {
-        logger::info("{} loading"sv, Version::PROJECT.data());
         try {
             setting::load_settings();
         } catch (const std::exception& e) { logger::warn("failed to load setting {}"sv, e.what()); }
-
-        Init(a_skse);
 
         switch (setting::get_log_level()) {
             case util::const_log_trace:
@@ -87,16 +54,68 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_s
                 spdlog::set_level(spdlog::level::info);
                 spdlog::flush_on(spdlog::level::info);
                 break;
+            default:
+                spdlog::set_level(spdlog::level::trace);
+                spdlog::flush_on(spdlog::level::trace);
+                break;
         }
+    } catch (const std::exception& e) { logger::critical("failed, cause {}"sv, e.what()); }
+}
 
-        auto messaging = SKSE::GetMessagingInterface();
-        if (!messaging->RegisterListener("SKSE", MessageHandler)) {
-            return false;
+EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse) {
+#ifndef NDEBUG
+    while (!IsDebuggerPresent()) {};
+#endif
+    REL::Module::reset();
+
+
+    init_logger();
+
+    logger::info("{} loading"sv, Version::PROJECT);
+
+    Init(a_skse);
+
+    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* a_msg) {
+        switch (a_msg->type) {
+            case SKSE::MessagingInterface::kDataLoaded:
+                logger::info("Data loaded"sv);
+                hook::install_hooks();
+                event::sink_event_handlers();
+                scaleform::Register();
+                logger::info("Done with adding"sv);
+                break;
         }
+    });
 
-        logger::info("{} loaded"sv, Version::PROJECT.data());
-    } catch (const std::exception& e) {
-        logger::critical("failed, cause {}"sv, e.what());
+    logger::info("{} loaded"sv, Version::PROJECT);
+    return true;
+}
+
+EXTERN_C [[maybe_unused]] __declspec(dllexport) constinit auto SKSEPlugin_Version = []() noexcept {
+    SKSE::PluginVersionData v;
+    v.PluginName(Version::PROJECT.data());
+    v.AuthorName(Version::AUTHOR);
+    v.PluginVersion({ Version::MAJOR, Version::MINOR, Version::PATCH, Version::BETA });
+    v.UsesAddressLibrary(true);
+    v.CompatibleVersions({ SKSE::RUNTIME_SSE_LATEST_SE });
+    return v;
+}();
+
+EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse,
+    SKSE::PluginInfo* pluginInfo) {
+    pluginInfo->name = SKSEPlugin_Version.pluginName;
+    pluginInfo->infoVersion = SKSE::PluginInfo::kVersion;
+    pluginInfo->version = SKSEPlugin_Version.pluginVersion;
+
+
+    if (a_skse->IsEditor()) {
+        logger::critical("Loaded in editor, marking as incompatible"sv);
+        return false;
+    }
+
+    const auto ver = a_skse->RuntimeVersion();
+    if (ver < SKSE::RUNTIME_SSE_1_5_39) {
+        logger::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
         return false;
     }
 
--- src/util/offset.h
+++ src/util/offset.h
@@ -1,6 +1,6 @@
 #pragma once
 
 namespace offset {
-    constexpr REL::ID get_relationship_rank{ 53898 }; //54710 might be for AE, testing
-    constexpr REL::ID get_dialog_menu{ 268589 };      //215255 AE
+    constexpr REL::RelocationID get_relationship_rank(53898, 54710);
+    constexpr REL::RelocationID get_dialog_menu(268589, 215255);
 };
